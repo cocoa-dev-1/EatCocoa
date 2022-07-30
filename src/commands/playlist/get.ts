eaed6660e1a443fdb6d8e3810a245a744dfd6a17
cocoa-dev-1 <sinmounbu0510@naver.com>
2022-07-31 03:52:07 +0900

    added: get playlist sys

--- src/commands/playlist/get.ts
+++ src/commands/playlist/get.ts
@@ -1,3 +1,29 @@
-import { EcCommand } from "../../types/command";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import Container from "typedi";
+import { PlayListManager } from "../../services/PlayListManager";
+import {
+  CommandCategory,
+  EcCommand,
+  EcCommandInteraction,
+} from "../../types/command";
 
-// export const getCommand: EcCommand = {};
+export const getCommand: EcCommand = {
+  name: "리스트검색",
+  description: "플레이 리스트를 찾습니다.",
+  category: CommandCategory.get("PLAYLIST").value,
+  data: new SlashCommandBuilder()
+    .setName("리스트검색")
+    .setDescription("플레이 리스트를 검색합니다.")
+    .addStringOption((option) =>
+      option
+        .setName("이름")
+        .setDescription("플레이 리스트 이름")
+        .setRequired(true)
+    )
+    .toJSON(),
+  async execute(interaction: EcCommandInteraction, guildId: string) {
+    await interaction.deferReply();
+    const playListManager = Container.get(PlayListManager);
+    const playListName = interaction.options.getString("이름");
+  },
+};
